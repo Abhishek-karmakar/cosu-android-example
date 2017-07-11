d3eae69ec98ab53ac7b602560b8bce430e1e4fe5
abhishek-karmakar <abhishek.karmakar.micromaxinfo.com>
2017-07-11 18:18:23 +0530

    Begin by editing required areas

--- app/src/main/java/com/abhishek_karmakar/cosu_sample/MainActivity.java
+++ app/src/main/java/com/abhishek_karmakar/cosu_sample/MainActivity.java
@@ -40,6 +40,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -80,13 +82,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_camera)
+        {
+            // lock the device here.
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_gallery)
+        {
+            // unlock the device here.
 
         } else if (id == R.id.nav_share) {
 
